1efdb76720381953eaf744d21fddbbed96d51f1e
Peter Kaukov <peter.kaukov@gmail.com>
2019-01-06 00:51:08 +0200

    Start working on hangman's header

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 build:
-	g++ src/main.cpp src/game/hangman.cpp src/message/message.cpp -o hangman
+	g++ src/main.cpp src/game/hangman.cpp -o hangman
--- include/hangman.hpp
+++ include/hangman.hpp
@@ -2,9 +2,61 @@
 
 #define HANGMAN_HANGMAN_HPP
 
+// #include <iostream>
+// #include <string>
+#include <vector>
+
 class Hangman {
   public:
     Hangman();
+    Hangman(std::string t_fileName);
+    std::string getPlaceholder();
+
+    bool getIsFinished() const
+    {
+      return m_isFinished;
+    }
+
+    void setLanguage(int t_languageId)
+    {
+      m_language = t_languageId;
+    }
+
+    int getLanguage() const
+    {
+      return m_language;
+    }
+
+    void printWelcome();
+
+    void prinRules() const
+    {
+      for (const std::string & rule : m_gameRules) {
+        std::cout << rule << std::endl;
+      }
+    }
+
+  private:
+    void setIsFinished(bool t_isFinished)
+    {
+      m_isFinished = t_isFinished;
+    }
+
+    void setWelcomeMessage(std::string t_welcomeMessage)
+    {
+      m_welcomeMessage = t_welcomeMessage;
+    }
+
+    void setGameRules(std::vector<std::string> t_gameRules)
+    {
+      m_gameRules = t_gameRules;
+    }
+
+    int m_language { 0 };
+    std::string m_placeholder { "" };
+    bool m_isFinished { false };
+    std::string m_welcomeMessage;
+    std::vector<std::string> m_gameRules;
 };
 
 #endif // HANGMAN_HANGMAN_HPP
