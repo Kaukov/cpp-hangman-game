fc9d3948ca56930591506cd4e99926c3558c55f1
Peter Kaukov <peter.kaukov@gmail.com>
2019-01-06 11:46:58 +0200

    Basic function definition for game

--- src/game/hangman.cpp
+++ src/game/hangman.cpp
@@ -1,17 +1,42 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <fstream>
+#include <locale>
 
 #include "../../include/hangman.hpp"
 
 using namespace std;
 
 Hangman::Hangman() {
-  m_fileName = "../../words.txt";
+  m_fileName = "words.txt";
+
+  initialize();
 }
 
 Hangman::Hangman(string t_fileName) {
   m_fileName = t_fileName;
+
+  initialize();
+}
+
+void Hangman::initialize() {
+  vector<string> gameRules = {
+    "1. Have fun!",
+    "2. Complete the game!"
+  };
+
+  string welcomeMessage = "Welcome to Hangman!";
+
+  setWelcomeMessage(welcomeMessage);
+  setGameRules(gameRules);
+
+  printWelcome();
+  printRules();
+
+  fillWords();
+
+  m_currentWord = getRandomWord();
 }
 
 bool Hangman::getIsFinished() const {
@@ -42,7 +67,7 @@ void Hangman::printWelcome() const {
   cout << m_welcomeMessage << endl;
 }
 
-void Hangman::prinRules() const {
+void Hangman::printRules() const {
   for (const string & rule : m_gameRules) {
     cout << rule << endl;
   }
@@ -58,4 +83,41 @@ void Hangman::setWelcomeMessage(string t_welcomeMessage) {
 
 void Hangman::setGameRules(vector<string> t_gameRules) {
   m_gameRules = t_gameRules;
+}
+
+void Hangman::fillWords() {
+  ifstream file;
+  string word;
+
+  file.open(m_fileName);
+
+  if (!file) {
+    cout << "Unable to open file: " << m_fileName << endl;
+    exit(1);
+  }
+
+  while(getline(file, word)) {
+    m_words.push_back(wordToLower(word));
+  }
+
+  file.close();
+}
+
+string Hangman::getRandomWord() const {
+  srand(time(nullptr));
+
+  int index = rand() % m_words.size() + 1;
+
+  return m_words[index];
+}
+
+string Hangman::wordToLower(string word) {
+  locale loc;
+  string wordToLower;
+
+  for (string::size_type i = 0; i < word.length(); i++) {
+    wordToLower += tolower(word[i], loc);
+  }
+
+  return wordToLower;
 }
