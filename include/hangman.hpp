eadafa2fe959d8255449de7b95e76cb684a271c3
Peter Kaukov <peter.kaukov@gmail.com>
2019-01-06 11:46:10 +0200

    Finish function declaration in game header

--- include/hangman.hpp
+++ include/hangman.hpp
@@ -9,7 +9,7 @@ class Hangman {
     Hangman();
     Hangman(std::string t_fileName);
     void printWelcome() const;
-    void prinRules() const;
+    void printRules() const;
     void setAllowedAttempts(int t_allowedAttempts);
     void setLanguage(int t_languageId);
     int getAllowedAttempts() const;
@@ -22,7 +22,10 @@ class Hangman {
     void setIsFinished(bool t_isFinished);
     void setWelcomeMessage(std::string t_welcomeMessage);
     void setGameRules(std::vector<std::string> t_gameRules);
-    void getRandomWord() const;
+    void fillWords();
+    void initialize();
+    std::string wordToLower(std::string word);
+    std::string getRandomWord() const;
 
     int                         m_languageId        { 0 };
     int                         m_attempts          { 0 };
@@ -33,7 +36,7 @@ class Hangman {
     std::string                 m_fileName          { "" };
     std::string                 m_welcomeMessage;
     std::vector<std::string>    m_gameRules;
-    // std::vector<std::string>    m_words;
+    std::vector<std::string>    m_words;
 };
 
 #endif // HANGMAN_HANGMAN_HPP
